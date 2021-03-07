d33e5db570aa0c57a4767366134eab5854b4be8c
Gabriel Oak <gabriel.costa41@fatec.sp.gov.br>
2021-03-07 02:55:14 -0300

    find by username doc

--- src/core/user/user-paths.ts
+++ src/core/user/user-paths.ts
@@ -1,40 +1,73 @@
 export default [
   {
-    "key": "/user",
-    "value": {
-      "get": {
-        "tags": [
+    key: "/user",
+    value: {
+      get: {
+        tags: [
           "User"
         ],
-        "description": "Get users paginated",
-        "parameters": [
+        description: "Get users paginated",
+        parameters: [
           {
-            "in": "query",
-            "name": "since",
-            "default": 0,
-            "type": "integer",
-            "required": false,
-            "description": "Minimal user id"
+            in: "query",
+            name: "since",
+            default: 0,
+            type: "integer",
+            required: false,
+            description: "Minimal user id"
           },
           {
-            "in": "query",
-            "name": "per_page",
-            "default": 10,
-            "type": "integer",
-            "required": false,
-            "description": "Quantity per page"
+            in: "query",
+            name: "per_page",
+            default: 10,
+            type: "integer",
+            required: false,
+            description: "Quantity per page"
           }
         ],
-        "responses": {
-          "200": {
-            "description": "OK",
-            "content": {
+        responses: {
+          200: {
+            description: "OK",
+            content: {
               "application/json": {
-                "schema": {
-                  "type": "array",
-                  "items": {
+                schema: {
+                  type: "array",
+                  items: {
                     "$ref": "#/definitions/User"
                   }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  },
+  {
+    key: "/user/:{username}",
+    value: {
+      get: {
+        tags: [
+          "User"
+        ],
+        description: "Get users paginated",
+        parameters: [
+          {
+            in: "path",
+            name: "username",
+            type: "string",
+            required: true,
+            description: "The github user name"
+          },
+        ],
+        responses: {
+          200: {
+            description: "OK",
+            content: {
+              "application/json": {
+                schema: {
+                  type: "object",
+                  "$ref": "#/definitions/User"
                 }
               }
             }
